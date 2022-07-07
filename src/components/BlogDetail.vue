b97ee83d68aeaa4b7042d4e12d859a7fd7e79ae9
SoominYim <soo15soo@kakao.com>
2022-07-07 11:25:28 +0900

    vue project

---
+++ src/components/BlogDetail.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h4>{{ blogs[$route.params.id].title }}</h4>
+        <p>{{ blogs[$route.params.id].date }}</p>
+        <h4>{{ blogs[$route.params.id].content }}</h4>
+        <a :href="blogs[$route.params.id].page" target="_blank"
+            >사이트보러가기</a
+        >
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "BlogDetail",
+    props: {
+        blogs: Array,
+    },
+};
+</script>
+
+<style>
+</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+// router를 사용하자
+import router from "./router.js";
 
-createApp(App).mount('#app')
+//use(router)로 이제 사용하면됨
+createApp(App).use(router).mount("#app");
