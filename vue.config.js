0ea6beb04f1915172f882e377038a395cefbcef3
SoominYim <soo15soo@kakao.com>
2024-01-31 11:01:36 +0900

    SourceMap Hide

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,11 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-    transpileDependencies: true,
-    publicPath: "/",
-    outputDir: "./docs",
+  transpileDependencies: true,
+  publicPath: "/",
+  outputDir: "./docs",
+  productionSourceMap: false,
+  configureWebpack: {
+    mode: "production",
+    devtool: "eval",
+  },
 });
