254864bd1e06ca581d215f9d487810cb8c4b18cb
SoominYim <soo15soo@kakao.com>
2024-03-05 10:26:19 +0900

    프로젝트 반응형 수정

--- src/assets/items/blog.js
+++ src/assets/items/blog.js
@@ -1,123 +1,123 @@
 export default [
-    {
-        number: 0,
-        title: "뷰스타그램",
-        content: "Vue로 만들어본 instagram",
-        project: `BaseLanguage : Vue3.X, javascript, scss 
+  {
+    number: 0,
+    title: "뷰스타그램",
+    content: "Vue로 만들어본 instagram",
+    project: `BaseLanguage : Vue3.X, javascript, scss 
         Server, DB : node.js, express, mySql
         Library, Plug-in : JWT-token, axios, Vuex, FaceBook Auth, instagram filterBox`,
-        gitUrl: "https://github.com/SoominYim/vuestagram",
-        url: null,
-        image: require("@/assets/img/vuestagram_01.png"),
-        contents: `
+    gitUrl: "https://github.com/SoominYim/vuestagram",
+    url: null,
+    image: require("@/assets/img/vuestagram_01.png"),
+    contents: `
         메인(로그인) 페이지
-        <img style="width : 600px;"src="${require("../img/vuestagram_01.png")}"/>
+        <img style="width : 100%;"src="${require("../img/vuestagram_01.png")}"/>
         회원가입 페이지
-        <img style="width : 600px;"src="${require("../img/vuestagram_02.png")}"/>
+        <img style="width : 100%;"src="${require("../img/vuestagram_02.png")}"/>
         로그인 후 피드
-        <img style="width : 420px;"src="${require("../img/vuestagram_03.png")}"/>
+        <img style="width : 100%;"src="${require("../img/vuestagram_03.png")}"/>
         기존 React로 만들어진 인스타그램을 Vue로 리팩토링하여 작업 
         `,
-    },
-    {
-        number: 1,
-        title: "Today-lunch",
-        content: "오늘 점심 뭐먹지",
-        project: `BaseLanguage : Vue3.X, javascript, scss
+  },
+  {
+    number: 1,
+    title: "Today-lunch",
+    content: "오늘 점심 뭐먹지",
+    project: `BaseLanguage : Vue3.X, javascript, scss
         Server, DB : null
         Library, Plug-in : Vuex`,
-        gitUrl: "https://github.com/SoominYim/today-lunch",
-        url: "https://soominyim.github.io/today-lunch",
-        image: require("@/assets/img/today-lunch.png"),
-        contents: `매일 점심 정하기가 귀찮아 만들어 본 랜덤 메뉴정하기
-        <img style="width : 600px;"src="${require("../img/today-lunch.png")}"/>
+    gitUrl: "https://github.com/SoominYim/today-lunch",
+    url: "https://soominyim.github.io/today-lunch",
+    image: require("@/assets/img/today-lunch.png"),
+    contents: `매일 점심 정하기가 귀찮아 만들어 본 랜덤 메뉴정하기
+        <img style="width : 100%;"src="${require("../img/today-lunch.png")}"/>
         원하는 음식이 나올때마다 누르는 한국인의 정서를 없애기 위해
         첫 째로 나온 음식이후엔 선택이 되지 않게 했다.
         랜덤 로직을 직접 짜는게 가장 오래 걸렸다.
         각 country로 랜덤으로 뽑아 배열의 푸시하는 방식으로 로직을 구성하였다.
 `,
-    },
-    {
-        number: 2,
-        title: "Todo",
-        content: `fire-base에 저장 되는 나만의 Todo List`,
-        project: `BaseLanguage : React, javascript, scss
+  },
+  {
+    number: 2,
+    title: "Todo",
+    content: `fire-base에 저장 되는 나만의 Todo List`,
+    project: `BaseLanguage : React, javascript, scss
         Server, DB : Firebase
         Library, Plug-in : googleAuth`,
-        gitUrl: "https://github.com/SoominYim/reactTodo",
-        url: null,
-        image: require("@/assets/img/reactTodoImg.png"),
-        contents: `초기 리액트 공부를 위해 만들었던 TODO LIST
+    gitUrl: "https://github.com/SoominYim/reactTodo",
+    url: null,
+    image: require("@/assets/img/reactTodoImg.png"),
+    contents: `초기 리액트 공부를 위해 만들었던 TODO LIST
         <img style="width : 600px;"src="${require("../img/reactTodoImg.png")}"/>
         `,
-    },
-    {
-        number: 3,
-        title: "Tetris",
-        content: `간단한 웹 테트리스`,
-        project: `BaseLanguage : Html, javascript, scss
+  },
+  {
+    number: 3,
+    title: "Tetris",
+    content: `간단한 웹 테트리스`,
+    project: `BaseLanguage : Html, javascript, scss
         Server, DB : Firebase
         Library, Plug-in : googleAuth,
         `,
-        gitUrl: "https://github.com/SoominYim/tetris",
-        url: "https://soominyim.github.io/tetris/",
-        image: require("@/assets/img/tetrisImg.png"),
-        contents: `공부를 위해 만들었던 테트리스.
-        <img style="width : 600px;"src="${require("../img/tetrisImg.png")}"/>
+    gitUrl: "https://github.com/SoominYim/tetris",
+    url: "https://soominyim.github.io/tetris/",
+    image: require("@/assets/img/tetrisImg.png"),
+    contents: `공부를 위해 만들었던 테트리스.
+        <img style="width : 100%;"src="${require("../img/tetrisImg.png")}"/>
         지금 봐도 낭만이 있다.
         Moving Blocks : ← →
         Block Change Direction : ↑
         Slow Drop: ↓
         Fast Drop : Space bar
         간단한 기능까지 구현 했다.`,
-    },
-    {
-        number: 4,
-        title: "vue-tetris",
-        content: "휴대용 게임기처럼 만든 테트리스",
-        project: `BaseLanguage : Vue2.X, javascript, less
+  },
+  {
+    number: 4,
+    title: "vue-tetris",
+    content: "휴대용 게임기처럼 만든 테트리스",
+    project: `BaseLanguage : Vue2.X, javascript, less
         Server, DB : null
         Library, Plug-in : Vuex,
         `,
-        gitUrl: "https://github.com/SoominYim/vue-tetris",
-        url: "https://soominyim.github.io/vue-tetris/",
-        image: require("@/assets/img/vue-tetris.png"),
-        contents: `기존의 구현했던 테트리스와는 다르게 좀 더 퀄리티 있는 
+    gitUrl: "https://github.com/SoominYim/vue-tetris",
+    url: "https://soominyim.github.io/vue-tetris/",
+    image: require("@/assets/img/vue-tetris.png"),
+    contents: `기존의 구현했던 테트리스와는 다르게 좀 더 퀄리티 있는 
         테트리스 게임을 만들고 싶었다.
-         <img style="width : 600px;"src="${require("../img/vue-tetris.png")}"/>
+         <img style="width : 100%;"src="${require("../img/vue-tetris.png")}"/>
         모바일에서도 즐길 수 있게 native하게 구성되었다.
         level, sound, pause, start Line 구성, 다음에 나올 block 등등의 기능들을 
         추가하여 온전한 미니게임으로 즐길 수 있다.
          `,
-    },
-    {
-        number: 5,
-        title: "jwt-login",
-        content: "jwt token 기반 로그인 기능 구현 project",
-        project: "node.js express project",
-        gitUrl: "https://github.com/SoominYim/jwt-login",
-        url: null,
-        image: "",
-        contents: "",
-    },
-    {
-        number: 6,
-        title: "socket-io",
-        content: "socket-io 를 이용한 채팅방 구현 project",
-        project: "vue node.js express project",
-        gitUrl: "https://github.com/SoominYim/vue-socket",
-        url: null,
-        image: "",
-        contents: "",
-    },
-    {
-        number: 7,
-        title: "vue-books",
-        content: "vue.js 2.X version 의 shopping mall project",
-        project: "vue project",
-        gitUrl: "https://github.com/SoominYim/vue-books",
-        url: null,
-        image: "",
-        contents: "",
-    },
+  },
+  {
+    number: 5,
+    title: "jwt-login",
+    content: "jwt token 기반 로그인 기능 구현 project",
+    project: "node.js express project",
+    gitUrl: "https://github.com/SoominYim/jwt-login",
+    url: null,
+    image: "",
+    contents: "",
+  },
+  {
+    number: 6,
+    title: "socket-io",
+    content: "socket-io 를 이용한 채팅방 구현 project",
+    project: "vue node.js express project",
+    gitUrl: "https://github.com/SoominYim/vue-socket",
+    url: null,
+    image: "",
+    contents: "",
+  },
+  {
+    number: 7,
+    title: "vue-books",
+    content: "vue.js 2.X version 의 shopping mall project",
+    project: "vue project",
+    gitUrl: "https://github.com/SoominYim/vue-books",
+    url: null,
+    image: "",
+    contents: "",
+  },
 ];
--- src/view/BlogProject.vue
+++ src/view/BlogProject.vue
@@ -1,195 +1,201 @@
 <template>
-    <div>
-        <div class="container" @click="goBack">
-        </div>
-        <div class="wrap">
-            <div class="scrolle">
-                <h1>{{ blogs[$route.params.id].title }}</h1>
-                <h2>{{ blogs[$route.params.id].content }}</h2>
-                <h4 v-html="project"></h4>
-                <div v-html="contents"></div>
-                <div class="btn-wrapper">
-                    <div class="btn-wrapper__container">
-                        <div class="btn-inner">
-                            <a class="btn-inner__text" :href="blogs[$route.params.id].gitUrl" target="_blank">view
-                                code</a>
-                        </div>
-                    </div>
-                </div>
-                <div class="btn-wrapper" v-if="blogs[$route.params.id].url !== null">
-                    <div class="btn-wrapper__container">
-                        <div class="btn-inner">
-                            <a class="btn-inner__text" :href="blogs[$route.params.id].url" target="_blank">preview</a>
-                        </div>
-                    </div>
-                </div>
+  <div>
+    <div class="container" @click="goBack"></div>
+    <div class="wrap">
+      <div class="scrolle">
+        <h1>{{ blogs[$route.params.id].title }}</h1>
+        <h2>{{ blogs[$route.params.id].content }}</h2>
+        <h4 v-html="project"></h4>
+        <div v-html="contents"></div>
+        <div class="btn-wrapper">
+          <div class="btn-wrapper__container">
+            <div class="btn-inner">
+              <a class="btn-inner__text" :href="blogs[$route.params.id].gitUrl" target="_blank">view code</a>
             </div>
+          </div>
         </div>
+        <div class="btn-wrapper" v-if="blogs[$route.params.id].url !== null">
+          <div class="btn-wrapper__container">
+            <div class="btn-inner">
+              <a class="btn-inner__text" :href="blogs[$route.params.id].url" target="_blank">preview</a>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
- 
+
 <script>
 export default {
-    name: "BlogProject",
-    props: {
-        blogs: Array,
-    },
+  name: "BlogProject",
+  props: {
+    blogs: Array,
+  },
 
-    computed: {
-        project() {
-            const blog = this.blogs[this.$route.params.id];
-            if (blog && blog.project) {
-                return blog.project.split('\n').join('<br/>');
-            }
-            return '';
-        },
-        contents() {
-            const blog = this.blogs[this.$route.params.id];
-            if (blog && blog.contents) {
-                return blog.contents.split('\n').join('<br/>');
-            }
-            return '';
-        }
+  computed: {
+    project() {
+      const blog = this.blogs[this.$route.params.id];
+      if (blog && blog.project) {
+        return blog.project.split("\n").join("<br/>");
+      }
+      return "";
     },
-    methods: {
-        goBack() {
-            this.$router.go(-1);
-        },
+    contents() {
+      const blog = this.blogs[this.$route.params.id];
+      if (blog && blog.contents) {
+        return blog.contents.split("\n").join("<br/>");
+      }
+      return "";
     },
-
+  },
+  methods: {
+    goBack() {
+      this.$router.go(-1);
+    },
+  },
 };
 </script>
 
 <style lang="scss" scoped>
-$blue: #5AC093;
-$btn-bg: #10131C;
+@import "@/assets/scss/utils.scss";
+$blue: #5ac093;
+$btn-bg: #10131c;
 
 .container {
-    height: 100vh;
-    width: 100vw;
-    z-index: 1;
-}
-
-img {
-    width: 420px;
+  height: 100vh;
+  width: 100vw;
+  z-index: 1;
 }
 
 h1 {
-    font-size: 6vw;
+  font-size: 6vw;
 }
 
 h2 {
-    font-size: 3vw;
+  font-size: 3vw;
 }
 
 .wrap {
-    position: absolute;
-    top: 0;
-    left: calc(50% - 80vw/2);
-    margin-top: 110px;
-    height: 80vh;
-    width: 80vw;
-    border-radius: 20px;
-    background: rgba(100, 100, 100, 0.2);
-    color: #fff;
-    z-index: 3;
-    line-height: 5vw;
+  position: absolute;
+  top: 0;
+  left: calc(50% - 80vw / 2);
+  margin-top: 110px;
+  height: 80vh;
+  width: 80vw;
+  border-radius: 20px;
+  background: rgba(100, 100, 100, 0.2);
+  color: #fff;
+  z-index: 3;
+  line-height: 5vw;
 }
 
 .scrolle {
-    box-sizing: border-box;
-    padding: 20px;
-    height: 80vh;
-    overflow-y: scroll;
-    overflow-x: hidden;
+  box-sizing: border-box;
+  padding: 30px 10px 10px 10px;
+  height: 80vh;
+  overflow-y: scroll;
+  overflow-x: hidden;
+}
+
+@include mobile {
+  .wrap {
+    position: absolute;
+    top: 0;
+    left: calc(50% - 95vw / 2);
+    margin-top: 60px;
+    height: 90vh;
+    width: 95vw;
+  }
+  .scrolle {
+    height: 90vh;
+  }
 }
 
 ::-webkit-scrollbar {
-    display: block;
-    background-color: rgba(32, 251, 182, 0.3);
-    border-radius: 50px;
-    width: 0.5em;
+  display: block;
+  background-color: rgba(32, 251, 182, 0.3);
+  border-radius: 50px;
+  width: 0.5em;
 }
 
 ::-webkit-scrollbar-thumb {
-    display: block;
-    background-color: rgba(90, 192, 147, 0.7);
-    border-radius: 50px;
+  display: block;
+  background-color: rgba(90, 192, 147, 0.7);
+  border-radius: 50px;
 }
 
 a {
-    color: #fff;
-    display: block;
-    font-size: 30px;
+  color: #fff;
+  display: block;
+  font-size: 30px;
 }
 
 /* BUTTON START */
 // Variables
 
-
-
 .btn-wrapper {
-    width: 290px;
-    height: 110px;
-    position: relative;
-    margin: 40px auto 0;
+  width: 290px;
+  height: 110px;
+  position: relative;
+  margin: 40px auto 0;
 
-    &:hover {
-        .btn-inner {
-            top: -4px;
-            transform: scale(1, 1);
-            cursor: pointer;
-        }
+  &:hover {
+    .btn-inner {
+      top: -4px;
+      transform: scale(1, 1);
+      cursor: pointer;
+    }
+  }
+
+  &__container {
+    border-bottom: 2px solid $blue;
+    position: absolute;
+    width: 100%;
+    height: 80px;
+
+    &:before,
+    &:after {
+      border-bottom: 2px solid $blue;
+      width: 96%;
+      left: 2%;
+      bottom: -8px;
+      content: "";
+      position: absolute;
     }
 
-    &__container {
-        border-bottom: 2px solid $blue;
-        position: absolute;
-        width: 100%;
-        height: 80px;
-
-        &:before,
-        &:after {
-            border-bottom: 2px solid $blue;
-            width: 96%;
-            left: 2%;
-            bottom: -8px;
-            content: "";
-            position: absolute;
-        }
-
-        &:after {
-            width: 92%;
-            left: 4%;
-            bottom: -14px;
-        }
-
-        .btn-inner {
-            width: 104.2%;
-            height: 100%;
-            position: absolute;
-            top: 20px;
-            left: -2.1%;
-            border: 2px solid $blue;
-            box-sizing: border-box;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            font-family: 'Source Code Pro', monospace;
-            letter-spacing: 1px;
-            text-transform: uppercase;
-            font-size: 18px;
-            background: $btn-bg;
-            transform: scale(.96, .96);
-            transition: all .3s;
-            z-index: 4;
-
-            &__text {
-                text-decoration: none;
-                color: $blue;
-            }
-        }
+    &:after {
+      width: 92%;
+      left: 4%;
+      bottom: -14px;
     }
+
+    .btn-inner {
+      width: 104.2%;
+      height: 100%;
+      position: absolute;
+      top: 20px;
+      left: -2.1%;
+      border: 2px solid $blue;
+      box-sizing: border-box;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      font-family: "Source Code Pro", monospace;
+      letter-spacing: 1px;
+      text-transform: uppercase;
+      font-size: 18px;
+      background: $btn-bg;
+      transform: scale(0.96, 0.96);
+      transition: all 0.3s;
+      z-index: 4;
+
+      &__text {
+        text-decoration: none;
+        color: $blue;
+      }
+    }
+  }
 }
 
 /* BUTTON END */
